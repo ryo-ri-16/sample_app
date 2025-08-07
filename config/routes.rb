3ecb9bd3d8207fcddd6838216021dd5b629ebfe4
ryo-ri-16 <ujiro1998@gmail.com>
2025-08-07 15:12:34 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,15 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, onky: [:create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
